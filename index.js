6b047cc2127e9d250f316bd48fa7301fd8d69684
DmitriyGovosyants <Demiangvs@gmail.com>
2022-05-26 19:58:27 +0300

    test

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 80;
 
+const todos = [
+    { "id": 1, "text": "Boob", "isActive": false },
+    { "id": 2, "text": "Name", "isActive": true },
+    { "id": 3, "text": "Liza", "isActive": false }
+]
+
 app.get('/', (req, res) => {
-    res.send(__dirname + '/src/data.json');
+    res.send(todos);
 })
 
 app.get('/trololo', (req, res) => {
